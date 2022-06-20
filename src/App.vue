a51717c046fd1f7489f5c3d26672879a1f8260bb
Kazuki Furukawa <6816398+karur4n@users.noreply.github.com>
2022-06-21 02:21:11 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+import Hey from "./components/Hey.vue";
+</script>
+
+<template>
+  <HelloWorld :msg="1" />
+  <Hey />
+</template>
